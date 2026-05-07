8680e31cf7ab44066de5de8c546f3ffa4c3aec2a
Peter Bočej <pbocej@gmail.com>
2026-05-07 11:43:01 +0200

    Add system controller

---
+++ WeatherAPI/Controllers/SysController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace WeatherAPI.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class SysController : ControllerBase
+   {
+      [HttpGet]
+      public async Task<IActionResult> Get()
+      {
+         return Ok(await Task.FromResult(new
+         {
+            Title = Assembly.GetEntryAssembly()?.GetName().Name,
+            Assembly.GetEntryAssembly()?.GetName().Version,
+            Clr = Environment.Version.ToString(),
+            OperatingSystem = Environment.OSVersion
+         }));
+      }
+   }
+}
