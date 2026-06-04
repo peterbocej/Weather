ff7b50f0706cc6e232b3048ccfc736de9356d64e
Peter Bočej <pbocej@gmail.com>
2026-06-04 15:18:38 +0200

    use SemaphoreSlim

--- Weather.Infrastructure/ExternalApi/ExternalWeatherApi.cs
+++ Weather.Infrastructure/ExternalApi/ExternalWeatherApi.cs
@@ -22,41 +22,47 @@ namespace Weather.Infrastructure.ExternalApi
       {
          var cityName = temperatureResult.City;
          var requestUrl = string.Format(_appSettings.WeatherApiServer.Url, _appSettings.WeatherApiServer.ApiKey, cityName);
-         using (var client = new HttpClient())
+         // Log the request URL for debugging purposes
+         _logger.LogInformation("Fetching weather data from API for city: {CityName} using URL: {RequestUrl}", cityName, requestUrl);
+         using (var semaphore = new SemaphoreSlim(1, 3))
          {
-            var response = await client.GetAsync(requestUrl);
-            response.EnsureSuccessStatusCode();
-            var content = await response.Content.ReadAsStringAsync();
-            var dict = JsonSerializer.Deserialize<Dictionary<string, object>>(content);
-            if (dict != null && dict.ContainsKey("current"))
+            await semaphore.WaitAsync();
+            using (var client = new HttpClient())
             {
-               var current = dict["current"] as JsonElement?;
-               if (current == null || !current.HasValue)
+               var response = await client.GetAsync(requestUrl);
+               response.EnsureSuccessStatusCode();
+               var content = await response.Content.ReadAsStringAsync();
+               var dict = JsonSerializer.Deserialize<Dictionary<string, object>>(content);
+               if (dict != null && dict.ContainsKey("current"))
                {
-                  _logger.LogError("Current weather data not found in API response for city: {CityName}", cityName);
-                  throw new Exception("Current weather data not found in API response.");
-               }
+                  var current = dict["current"] as JsonElement?;
+                  if (current == null || !current.HasValue)
+                  {
+                     _logger.LogError("Current weather data not found in API response for city: {CityName}", cityName);
+                     throw new Exception("Current weather data not found in API response.");
+                  }
 
-               if (current.Value.TryGetProperty("temp_c", out var temp_c))
-                  temperatureResult.TemperatureC = temp_c.GetDouble();
+                  if (current.Value.TryGetProperty("temp_c", out var temp_c))
+                     temperatureResult.TemperatureC = temp_c.GetDouble();
+                  else
+                  {
+                     _logger.LogError("Temperature data not found in API response for city: {CityName}", cityName);
+                     throw new Exception("Temperature data not found in API response.");
+                  }
+
+                  if (!(current.Value.TryGetProperty("last_updated", out var last_updated)
+                     && DateTime.TryParse(last_updated.GetString(), out var lastUpdated)))
+                  {
+                     _logger.LogError("Last updated data not found in API response for city: {CityName}", cityName);
+                     throw new Exception("Last updated data not found in API response.");
+                  }
+                  temperatureResult.MeasuredAtUTC = lastUpdated.ToUniversalTime();
+                  return true;
+               }
                else
                {
-                  _logger.LogError("Temperature data not found in API response for city: {CityName}", cityName);
-                  throw new Exception("Temperature data not found in API response.");
+                  throw new Exception("Invalid response from weather API.");
                }
-
-               if (!(current.Value.TryGetProperty("last_updated", out var last_updated)
-                  && DateTime.TryParse(last_updated.GetString(), out var lastUpdated)))
-               {
-                  _logger.LogError("Last updated data not found in API response for city: {CityName}", cityName);
-                  throw new Exception("Last updated data not found in API response.");
-               }
-               temperatureResult.MeasuredAtUTC = lastUpdated.ToUniversalTime();
-               return true;
-            }
-            else
-            {
-               throw new Exception("Invalid response from weather API.");
             }
          }
       }
