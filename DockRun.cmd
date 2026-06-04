c1281093ab15e3bda5badca05a0feecf47ba5157
Peter Bočej <pbocej@gmail.com>
2026-06-04 02:26:20 +0200

    remove docker batch files

--- DockRun.cmd
+++
@@ -1,6 +0,0 @@
-docker build -t weatherapi .
-docker run -d -p 23232:8080 -p 23233:8081 --name CWeatherApi weatherapi:latest -e ASPNETCORE_URLS="https://+;http://+" -e ASPNETCORE_HTTPS_PORT=23233 -v $env:APPDATA\microsoft\UserSecrets\:C:\Users\ContainerUser\AppData\Roaming\microsoft\UserSecrets -v $env:USERPROFILE\.aspnet\https:C:\Users\ContainerUser\AppData\Roaming\ASP.NET\Https
-
-docker images -a
-docker container list -a
-docker container logs CWeatherApi -f
--- DockStop.cmd
+++
@@ -1,6 +0,0 @@
-docker container stop CWeatherApi
-docker container rm CWeatherApi
-docker image rm weatherapi:latest
-
-docker images -a
-docker container list -a
